8c00c5621ad4df86cc62f6c93edcbf1fd251818b
realDrayde <morozed@student.uzlabina.cz>
2022-05-25 15:23:09 +0200

    getting data from API and writing it out
    getting data from API and writing it out
    (janky)

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # basedInvoice
- <h1>K2 invoice reader app</h1>
-dis bich be bussin'
+ <h1>K2 invoice reader app thing</h1>
+i wanna die wanna die
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+// links
+let urlMain = "https://k2demo.abis.cz:44302/V22K2_API_DEMO/Data/TInvoiceOutDM?fields=TradingPartnerId,DocumentIdentificationCalc,AmountGrossC,Currency&conditions=TradingPartnerId;EQ;";
 // Table values
-const headers = ["Document ID", "Description", "Amount Gross", "Currency"];
-const cols = 4;
+const headers = ["Document ID", "Date", "Amount Gross", "Currency"];
+let docID = [], date = [], amgross = [], curr = [];
+let cols = 4;
 // Access to HTML elements
 const form = document.getElementById('inputForm');
 const actionButtons = document.getElementsByClassName('action-button');
@@ -9,7 +12,39 @@ const ID_INVALID = "Invalid Trading Partner ID";
 const DATE_INVALID = "Invalid date";
 // Input and output variables
 let inTPID, inStartDate, inEndDate, inCount;
-let outName, outAbbr, outAdd;
+let outTPID, outName, outAbbr, outAdd;
+let data, subdata;
+// Function for communicating with API
+async function CallAPI(url) {
+    let response = await fetch(url);
+    let object = await response.json();
+    let i = 0;
+    console.log("url: " + url + "\nresponse: " + response + "\nobject: " + object);
+    object = object.Items;
+    await Promise.all(object.map( async (x) =>{
+        let partnerUrl = x.FieldValues[3].Value.ItemURL;
+        let subresponse = await fetch(partnerUrl);
+        let subobject = await subresponse.json();
+        //inCount = x.RecordsCount;
+        outName = subobject.FieldValues[3].Value;
+        outAbbr = x.FieldValues[3].Value.FieldValues[0].Value;
+        outTPID = subobject.FieldValues[5].Value;
+        docID[i] = x.FieldValues[1].Value;
+        date[i] = x.FieldValues[4].Value;
+        amgross[i] = x.FieldValues[0].Value;
+        curr[i] = x.FieldValues[3].Value.FieldValues[1].Value;
+        //console.log("i: " + i + " partnerName: " + outName + " abbr: " + outAbbr + " docID: " + docID[i] + " desc: " + date[i] + " amgross: " + amgross[i] + " curr: " + curr[i] + "\n");
+        //console.log("count: " + inCount);
+        i++;
+    }))
+    inCount = i;
+}
+async function CallAPIsec(url) {
+    let response = await fetch(url);
+    let object = await response.json();
+    console.log("url: " + url + "\nresponse: " + response + "\nobject: " + object);
+    subdata = object.Items;
+}
 // Validating the ID input (is numeric?)
 function ValidateID(input, invalidMsg) {
     if (isNaN(input)) {
@@ -37,13 +72,17 @@ function ReadInput() {
     let dateValid = ValidateDates();
     if (idValid && dateValid) {
         inTPID = document.getElementById('inTPID').value;
-        inStartDate = document.getElementById('inDateFrom').value;
-        inEndDate = document.getElementById('inDateTo').value;
-        inCount = document.getElementById('inCount').value;
+        //inStartDate = document.getElementById('inDateFrom').value;
+        //inEndDate = document.getElementById('inDateTo').value;
+        //inCount = document.getElementById('inCount').value;
+        let gigachad = urlMain + inTPID;
+        console.log(gigachad);
+        CallAPI(gigachad);
         return true;
     } else {
         return false;
     }
+
 }
 // Generating the table based on how many rows the user wants (inCount)
 function CreateTable(rows) {
@@ -79,15 +118,45 @@ function CreateTable(rows) {
             let tableCol = document.createElement('td');
             tableCol.setAttribute('id','col'+j);
             tableRow.appendChild(tableCol);
-            tableCol.appendChild(document.createTextNode(i + " " + j));
+            //tableCol.appendChild(document.createTextNode(i + " " + j));
+            switch (j) {
+                case 0:
+                    tableCol.appendChild(document.createTextNode(docID[i]));
+                    break;
+                case 1:
+                    tableCol.appendChild(document.createTextNode(date[i]));
+                    break;
+                case 2:
+                    tableCol.appendChild(document.createTextNode(amgross[i]));
+                    break;
+                case 3:
+                    tableCol.appendChild(document.createTextNode(curr[i]));
+                    break;
+            }
             //tableCol.appendChild(document.createTextNode(data));
         }
     }
 }
 // Updating data
 function PrintTPinfo() {
-    document.getElementById('outTPID').innerHTML = "ID: " + inTPID;
+    document.getElementById('outTPID').innerHTML = "ID: " + outTPID;
     document.getElementById('outName').innerHTML = "Name: " + outName;
     document.getElementById('outAbbr').innerHTML = "Abbreviation: " + outAbbr;
     document.getElementById('outAdd').innerHTML = "Address: " + outAdd;
 }
+
+// BRAZIL
+/*
+async function callAPI2() {
+    (async () => {
+        let response = await fetch(urlMain);
+        let object = await response.json();
+        //object.map(x =>{
+        //}) // Tohle je od Martina
+        //console.log(object);
+        alert(object);
+        return object;
+    })();
+}
+
+ */
