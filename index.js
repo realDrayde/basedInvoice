53ea9f3aac02d8c85572f746c1b296a5bf65ea1b
rajusus <105707289+rajusus@users.noreply.github.com>
2022-05-25 12:19:20 +0200

    ftz

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 let urlMain = "https://k2demo.abis.cz:44302/V22K2_API_DEMO/Data/TInvoiceOutDM?fields=TradingPartnerId,DocumentIdentificationCalc,AmountGrossC,Currency&conditions=TradingPartnerId;EQ;21";
-const infoAmmount = 4;
+let id;
+let abbr;
+let name;
+let address;
 
 async function callApi() {
-
     const request = new Request(urlMain);
 
     const response = await fetch(request);
     const object = await response.json();
 
+    object.map(x =>{
+
+    id = x.FieldValues[6].Value;
+
+    })
+
+
     populate(object);
     console.log(object);
 }
 
+// random řádek [insert js code]
+
 function populate(obj) {
+
+
     //Tady bude kód, který vypíše hodnoty z API do UI
 
     const myPara = document.createElement('p');
@@ -21,9 +34,24 @@ function populate(obj) {
 
     //myPara.textContent = `Hometown: ${obj['homeTown']} // Formed: ${obj['formed']}`;
     //header.appendChild(myPara);
+
+
 }
 
+/* Uložím si sem
+    (async () => {
+        let response = await fetch(urlMain);
+        let user = await response.json();
 
+
+
+        console.log(items);
+
+        }
+
+    )
+
+     */
 
 
 
@@ -73,5 +101,33 @@ function populateHeroes(obj) {
         section.appendChild(myArticle);
     }
 }*/
+
+async function populate() {
+
+    const requestURL = 'https://k2demo.abis.cz:44302/V22K2_API_DEMO/Data/TInvoiceOutDM?fields=TradingPartnerId,DocumentIdentificationCalc,AmountGrossC,Currency&conditions=TradingPartnerId;EQ;21';
+    const request = new Request(requestURL);
+
+    const response = await fetch(request);
+    const partners = await response.json();
+
+    populateHeader(partners);
+    populateHeroes(partners);
+
+}
+
+function populateHeader(obj) {
+    const header = document.querySelector('header');
+    const myH1 = document.createElement('h1');
+    myH1.textContent = obj['partnerName'];
+    header.appendChild(myH1);
+
+    const myPara = document.createElement('p');
+    myPara.textContent = `Partner: ${obj['']} // Formed: ${obj['formed']}`;
+    header.appendChild(myPara);
+}
+
+
+
+
 
 
